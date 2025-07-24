4aa37e00f19b3253d0be06437e56e474ae5648b9
Jaka Brajadenta <79687223+jakabrajadenta@users.noreply.github.com>
2025-07-24 14:07:59 +0700

    Init project

---
+++ src/main/java/co/id/jalin/jbpm_explore/JBpmExploreApplication.java
@@ -0,0 +1,13 @@
+package co.id.jalin.jbpm_explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBpmExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBpmExploreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/id/jalin/jbpm_explore/JBpmExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.id.jalin.jbpm_explore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBpmExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
